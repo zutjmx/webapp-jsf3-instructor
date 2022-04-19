f4c9cf772415ad059707516d73b2fe35edb053c7
Jesus Zuñiga <jesuszt@hotmail.com>
2022-04-19 11:04:36 -0500

    Fin de la sección 32 19/04/2022

--- src/main/java/org/zutjmx/webapp/jsf3/controllers/ProductoController.java
+++ src/main/java/org/zutjmx/webapp/jsf3/controllers/ProductoController.java
@@ -3,6 +3,8 @@ package org.zutjmx.webapp.jsf3.controllers;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.inject.Model;
 import jakarta.enterprise.inject.Produces;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import org.zutjmx.webapp.jsf3.entities.Categoria;
@@ -20,6 +22,9 @@ public class ProductoController {
 
     @Inject
     private ProductoService service;
+
+    @Inject
+    private FacesContext facesContext;
 
     @Produces
     @Model
@@ -59,12 +64,18 @@ public class ProductoController {
 
     public String guardar() {
         System.out.println(producto);
+        if (producto.getId() != null && producto.getId()>0) {
+            facesContext.addMessage(null,new FacesMessage("Producto " + producto.getNombre() + " actualizado con éxito."));
+        } else {
+            facesContext.addMessage(null,new FacesMessage("Producto " + producto.getNombre() + " creado con éxito."));
+        }
         service.guardar(producto);
         return "index.xhtml?faces-redirect=true";
     }
 
-    public String eliminar(Long id) {
-        service.eliminar(id);
+    public String eliminar(Producto producto) {
+        service.eliminar(producto.getId());
+        facesContext.addMessage(null,new FacesMessage("Producto " + producto.getNombre() + " eliminado con éxito."));
         return "index.xhtml?faces-redirect=true";
     }
 
